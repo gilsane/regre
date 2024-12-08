0c8c97aa49f84a213b33153c422d2302e92ee789
gilsane <20244444@sdhs.gwe.hs.kr>
2024-12-08 22:42:43 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,11 @@ def load_model(file_id, model_type):
     elif model_type == "sklearn":
         # Scikit-learn 모델과 메타정보 로드
         with open(output, 'rb') as f:
-            model_metadata = pickle.load(f)
-        assert isinstance(model_metadata, dict), "로드된 데이터가 사전 형태가 아닙니다!"
+            try:
+                data = pickle.load(f)
+                print(type(data))
+            except Exception as e:
+                print(f"파일 로드 실패: {e}")
 
     else:
         raise ValueError(f"알 수 없는 모델 타입: {model_type}")
